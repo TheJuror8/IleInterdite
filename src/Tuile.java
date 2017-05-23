1e18fee683d821d70167061a10e8d069660d0165
BARNEOUD-ARNAUD Lucas <barneoul@hostname.iut2.upmf-grenoble.fr>
2017-05-23 14:16:06 +0200

    Ajouts dans Tuile

--- src/Tuile.java
+++ src/Tuile.java
@@ -1,43 +1,48 @@
-import java.util.*;
+package ileinterdite;
+
+import java.util.ArrayList;
 
 public class Tuile {
 
-	private Collection<Aventurier> joueurSurCase;
+	private ArrayList<Aventurier> joueursSurCase;
 	private EtatTuile etatTuile;
 	private int posX;
 	private int posY;
+        private String nom;
+    
+    public Tuile(String nom, int posX, int posY){
+        this.nom = nom;
+        this.posX = posX;
+        this.posY = posY;
+        joueursSurCase = new ArrayList();
+        etatTuile = EtatTuile.Sèche;
+    }
+
+        public int getX() {
+            return posX;
+        }
+
+        public int getY() {
+            return posY;
+        }
+
+
 
 	public int[] getPosition() {
-		// TODO - implement Tuile.getPosition
-		throw new UnsupportedOperationException();
+		int[] pos = new int[2];
+                
+                pos[0] = getX();
+                pos[1] = getY();
+                
+                return pos;
 	}
 
-	/**
-	 * 
-	 * @param j
-	 */
+
+        
 	public void addJoueurCase(Aventurier j) {
-		// TODO - implement Tuile.addJoueurCase
-		throw new UnsupportedOperationException();
+		joueursSurCase.add(j);
 	}
 
-	public void afficherCase() {
-		// TODO - implement Tuile.afficherCase
-		throw new UnsupportedOperationException();
-	}
 
-	/**
-	 * 
-	 * @param etat
-	 */
-	public void setEtat(EtatTuile etat) {
-		// TODO - implement Tuile.setEtat
-		throw new UnsupportedOperationException();
-	}
-
-	public EtatTuile getEtat() {
-		// TODO - implement Tuile.getEtat
-		throw new UnsupportedOperationException();
-	}
 
 }
